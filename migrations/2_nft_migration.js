622e68e8ac890a75c3a6aba3e9232e328d1a9478
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-26 22:37:48 +0400

    added config vars to migration

--- migrations/2_nft_migration.js
+++ migrations/2_nft_migration.js
@@ -3,6 +3,5 @@ const { PRICE, NAME, VERSION } = require('../nft.config.js');
 
 module.exports = async function (deployer) {
   const [admin] = await web3.eth.getAccounts();
-  const price = await web3.utils.toWei('1', 'ether');
   const nft = await deployer.deploy(NFT, admin, NAME, VERSION, PRICE);
 };
